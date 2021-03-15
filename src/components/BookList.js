48abac379c65c765c8045f47fd9bef3b35738c46
Julie J <jeannet.julie@gmail.com>
2021-03-15 16:53:13 +0100

    Afficherer tous les livres à l'aide de Grid et Segment

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Book from "./BookCard";
+// sous forme de components fournis par semantic-ui, pour l'affichage en grid
+import { Grid, Segment } from "semantic-ui-react";
 
 export default function BookList() {
   const [books, setBooks] = useState([]);
@@ -20,11 +22,24 @@ export default function BookList() {
         // <pre>
         //   <code>{JSON.stringify(books, null, 4)}</code>
         // </pre>
-                <div>
-                {books.map(b => (
-                  <Book data={b} key={b.id} />
-                ))}
-              </div>
+
+        // puisque on va utiliser l'affichage en grid, on remplace les lignes suivantes
+              //   <div>
+              //   {books.map(b => (
+              //     <Book data={b} key={b.id} />
+              //   ))}
+              // </div>
+
+              // grid basée sur https://github.com/Semantic-Org/Semantic-UI-React/blob/master/docs/src/layouts/ResponsiveLayout.js
+              <Grid columns={3} doubling stackable>
+              {books.map(b => (
+                <Grid.Column key={b.id}>
+                  <Segment>
+                    <Book data={b} />
+                  </Segment>
+                </Grid.Column>
+              ))}
+            </Grid>
       )}
     </>
   );
