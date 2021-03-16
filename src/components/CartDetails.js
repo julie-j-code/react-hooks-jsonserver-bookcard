9be071af7b5cc5b753d8eb61e9df613392b01566
Julie J <jeannet.julie@gmail.com>
2021-03-16 14:23:03 +0100

    Appeler addToCard() depuis le detail du caddie

--- src/components/CartDetails.js
+++ src/components/CartDetails.js
@@ -3,42 +3,45 @@ import { CartContext } from "../App";
 import { Header, Table, Icon } from "semantic-ui-react";
 
 export default function CartDetails() {
-  const { cart } = useContext(CartContext);
-  return (
-    <>
-      {/* <div>{JSON.stringify(cart, null, 4)}</div> */}
+    // à récupérer depuis le context
+    const { cart, addToCart, removeFromCart } = useContext(CartContext);
+    return (
+        <>
+            {/* <div>{JSON.stringify(cart, null, 4)}</div> */}
 
-      <Table celled padded>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell singleLine textAlign="center">
-              Quantité
+            <Table celled padded>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell singleLine textAlign="center">
+                            Quantité
             </Table.HeaderCell>
-            <Table.HeaderCell>Titre</Table.HeaderCell>
-            <Table.HeaderCell textAlign="right">Prix unitaire</Table.HeaderCell>
-            <Table.HeaderCell textAlign="right">Prix total</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
+                        <Table.HeaderCell>Titre</Table.HeaderCell>
+                        <Table.HeaderCell textAlign="right">Prix unitaire</Table.HeaderCell>
+                        <Table.HeaderCell textAlign="right">Prix total</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
 
-        <Table.Body>
-          {Object.keys(cart).map(key => (
-            //   ne pas oublier d'ajouter une clée unique pour chaque row
-            <Table.Row key={cart[key].id}>
-              <Table.Cell>
-                <Header as="h2" textAlign="center">
-                  {cart[key].quantity}
-                </Header>
+                <Table.Body>
+                    {Object.keys(cart).map(key => (
+                        //   ne pas oublier d'ajouter une clée unique pour chaque row
+                        <Table.Row key={cart[key].id}>
+                            <Table.Cell>
+                                <Header as="h2" textAlign="center">
+                                    <button onClick={() => removeFromCart(cart[key])}>-</button>{" "}
+                                    <button onClick={() => addToCart(cart[key])}>+</button>{" "}
+                                    {cart[key].quantity}
+                                </Header>
+                            </Table.Cell>
+                            <Table.Cell singleLine>{cart[key].title}</Table.Cell>
+                            <Table.Cell textAlign="right">{cart[key].price} €</Table.Cell>
+                            <Table.Cell textAlign="right">
+                                {cart[key].quantity * cart[key].price} €
               </Table.Cell>
-              <Table.Cell singleLine>{cart[key].title}</Table.Cell>
-              <Table.Cell textAlign="right">{cart[key].price} €</Table.Cell>
-              <Table.Cell textAlign="right">
-                {cart[key].quantity * cart[key].price} €
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table>
 
-    </>
-  );
+        </>
+    );
 }
